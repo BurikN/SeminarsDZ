3ff8d3391321280c21789924c8027190359183f9
Nikita <tesh-123@mail.ru>
2022-09-11 23:33:53 +0300

    Добавили дз по 1 семинару

---
+++ DZ/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача2
+Console.Write("Введите первое число,");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число,");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+ {
+    Console.Write("Большее число:");
+    Console.WriteLine(number1);
+    Console.Write("Меньшее число:");
+    Console.WriteLine(number2);
+ }
+if (number2 > number1)
+{
+    Console.Write("Большее число:");
+    Console.WriteLine(number2);
+    Console.Write("Меньшее число:");
+    Console.WriteLine(number1);
+}
+*/
+/*Задача 4
+Console.Write("Введите первое число,");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число,");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число,");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max) 
+max = number2;
+if (number3 > max) 
+max = number3;
+
+Console.Write("Максимальное число:");
+Console.WriteLine(max);
+*/
+/*Задача 6
+Console.Write("Введите число,");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
+*/
+/*Задача 8
+Console.Write("Введите число,");
+int num = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<=num)
+{
+    if (i % 2==0)
+    Console.Write(i+" ");
+    i++;
+}
+*/
+
+
